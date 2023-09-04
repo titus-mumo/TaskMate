e137d9c553a598725d76c92cf5fce7219dcbdc71
Titus Mumo <113600628+titus-mumo@users.noreply.github.com>
2023-09-04 12:47:11 +0300

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -58,7 +58,7 @@ function TaskList({ tasks, setTasks, update, setUpdate, updateString, setUpdateS
                 <h1 className={styles.title} >Task List</h1>
                 <button onClick={handleShow} type='button' className="btn btn-primary">{ show? 'Hide':'Show'}</button>
             </div>
-            {loading && <p>Loading products... </p>}
+            {loading && <p>Add a task... </p>}
             <ul>
                 { show && tasks.map((task) => (
                     <li key={task.id} className={task.completed? 'completed': 'incompleted'}>
